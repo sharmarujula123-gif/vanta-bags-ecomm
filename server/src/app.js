0774a1fb72586042542889ab2e35688d1d9f9775
Rujula Sharma <sharmarujula123@gmail.com>
2026-08-08 20:47:40 +0530

    chore: set up backend server and database

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,28 @@
-s
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Vanta Bags API is running",
+  });
+});
+
+export default app;
---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Vanta Bags API running on port ${PORT}`);
+  });
+};
+
+startServer();
